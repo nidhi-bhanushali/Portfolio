b3c589a5c6c1970acfcc4fca5438e71a07e16429
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2020-12-03 02:01:26 +0530

    Added Navbar And Routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import logo from "./logo.svg";
 import "./css/App.css";
-import React from "react";
+import React , { Fragment } from "react";
+import {BrowserRouter as Router , Route , Switch } from 'react-router-dom'
+import Navbar from "./layout/Navbar";
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar/>
+        <Switch>
+          <Fragment>
+            <Route exact path = '/' component = {Home} />
+            <Route exact path = '/home' component = {Home} />
+            <Route exact path = '/about' component = {About} />
+            <Route exact path = '/contact' component = {Contact} />
+          </Fragment>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <div className = 'navbar bg-primary'>
+            <ul>
+                <Link to = '/'>Nidhi Bhanushali</Link>
+                <Link to = '/home'>Home</Link>
+                <Link to = '/about'>About</Link>
+                <Link to = '/contact'>Contact</Link>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
