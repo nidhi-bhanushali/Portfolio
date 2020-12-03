e5061b356543e8670de71f1cedf942998ca1fb04
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2020-12-04 01:52:32 +0530

    Updated Profile Data

---
+++ src/profileData.js
@@ -0,0 +1,42 @@
+export const Data = {
+    img:"../../public/images/IMG-20191123-WA0121.jpg",
+    alt:"photo",
+    name:"Nidhi Bhanushali",
+    college:"KJSIEIT-2022 , Mumbai University",
+    desc:"Learning various web technologies and applying them to make smaller projects. Currently Interning at Travographer",
+}
+
+export const ProjectList = [
+    {
+        name:"Stopwatch using React",
+        link:"https://1kp4m.csb.app/"
+    },
+    {
+        name:"Github Finder using React",
+        link:"https://githubfinder6754764.netlify.app/"
+    },
+    {
+        name:"Expense Tracker using Vanilla JS",
+        link:"https://brave-hodgkin-a23f43.netlify.app/"
+    },
+    {
+        name:"To Do List using React",
+        link:"https://get-it-done868789.netlify.app"
+    }
+]
+
+export const BlogList = [
+    {
+
+        name: "Lorem, ipsum dolor sit amet consectetur!",
+    },
+    {
+        name:"Lorem, ipsum dolor sit amet consectetur!"
+    },
+    {
+        name:"Lorem, ipsum dolor sit amet consectetur!"
+    },
+    {
+        name:"Lorem, ipsum dolor sit amet consectetur!"
+    }
+]
