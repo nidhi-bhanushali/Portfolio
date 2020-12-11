667a53c191e4158a51ee9fc1c554b91bac35a9bf
Nidhi Bhanushali <bhanushalinidhi44@gmail.com>
2020-12-11 19:59:35 +0530

    Added About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Data} from '../profileData'
 
 const About = () => {
     return (
-        <div>
-            <h1>This is about page</h1>
+        <div className='container'>
+            <h3 className='mt all-center'>About Me</h3>
+            <p className='text-light'>{Data.bio}</p>
         </div>
     )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -12,6 +12,9 @@ const Home = () => {
             <div>
             <h4 className = "large text-primary my-2">{Data.name}</h4>
             <p className = "text-light">{Data.desc}</p>
+            <a href = 'https://github.com/nidhi-bhanushali'><i class="social-icon fab fa-github"></i></a>
+            <a href = 'https://www.linkedin.com/in/nidhi-bhanushali-40287217a'><i class="social-icon fab fa-linkedin"></i></a>
+            
             <br>
             </br>
            
